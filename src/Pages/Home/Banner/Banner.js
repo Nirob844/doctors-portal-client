b410109eacb9a01876d350f881a1224e42e5b446
Nirob Hasan <nirob427417@gmail.com>
2023-02-18 17:55:35 +0600

    info cards and services

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -9,7 +9,7 @@ const Banner = () => {
                 <div>
                     <h1 className="text-5xl font-bold">Box Office News!</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button>Getting Started</button>
+                    <button className='bg-gradient-to-r from-primary to-secondary text-white'>Getting Started</button>
                 </div>
             </div>
         </div>
