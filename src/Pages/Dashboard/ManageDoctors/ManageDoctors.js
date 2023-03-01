55c513744988dcf0c00b0f7645b6e497b2e603eb
Nirob Hasan <nirob427417@gmail.com>
2023-03-01 19:31:48 +0600

    manage doctor

--- src/Pages/Dashboard/ManageDoctors/ManageDoctors.js
+++ src/Pages/Dashboard/ManageDoctors/ManageDoctors.js
@@ -1,8 +1,82 @@
 import React from 'react';
+import { toast } from 'react-hot-toast';
+import { useQuery } from 'react-query';
+import Loading from '../../Shared/Loading/Loading';
 
 const ManageDoctors = () => {
+
+    const { data: doctors, isLoading, refetch } = useQuery({
+        queryKey: ['doctors'],
+        queryFn: async () => {
+            try {
+                const res = await fetch('http://localhost:5000/doctors', {
+                    headers: {
+                        authorization: `bearer ${localStorage.getItem('accessToken')}`
+                    }
+                });
+                const data = await res.json();
+                return data;
+            }
+            catch (error) {
+
+            }
+        }
+    });
+
+    const handleDeleteDoctor = doctor => {
+        fetch(`http://localhost:5000/doctors/${doctor._id}`, {
+            method: 'DELETE',
+            headers: {
+                authorization: `bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount > 0) {
+                    refetch();
+                    toast.success(`Doctor ${doctor.name} deleted successfully`)
+                }
+            })
+    }
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
+
     return (
         <div>
+            <h2 className="text-3xl">Manage Doctors: {doctors?.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Avatar</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Specialty</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            doctors.map((doctor, i) => <tr key={doctor._id}>
+                                <th>{i + 1}</th>
+                                <td><div className="avatar">
+                                    <div className="w-24 rounded-full">
+                                        <img src={doctor.image} alt="" />
+                                    </div>
+                                </div></td>
+                                <td>{doctor.name}</td>
+                                <td>{doctor.email}</td>
+                                <td>{doctor.specialty}</td>
+
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
 
         </div>
     );
